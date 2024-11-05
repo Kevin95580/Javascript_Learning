dba505abf30ba9e9402300a91af791c40d1778ed
Kevin95580 <163616303+Kevin95580@users.noreply.github.com>
2024-11-05 05:07:57 +0000

    datatypes done

---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,15 @@
+"use strict" ; // treat all js as newer version
+
+let name = "Shingham";
+let number = 123
+let isLoggedIn = true;
+
+//console.log(typeof variablename/variablevalue);
+
+console.log(typeof number);
+
+
+
+
+//typeof null => object (it is type of null or error of js)
+//typeof undefined => undefined 
