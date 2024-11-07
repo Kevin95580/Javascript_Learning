80bbb4d85c8821c654b5810765bf31cfd2d8e6a0
Kevin95580 <163616303+Kevin95580@users.noreply.github.com>
2024-11-07 11:55:24 +0000

    comparisons done

---
+++ 01_Basics/04_Comparison.js
@@ -0,0 +1,19 @@
+console.log("2">1);
+console.log("02">1);
+
+console.log(null > 0);
+console.log(null >= 0);
+console.log(null == 0);
+console.log(null < 0);
+console.log(null <= 0);
+
+//all comparisons like ==,!=,>,<,>=,<= works differently
+//it converts null to a 0 and hence the output of >=,<= is true
+
+console.log("++++++++++++++++++++++++++++++");
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined < 0); 
+console.log(undefined >= 0);
+console.log(undefined >= 0);
