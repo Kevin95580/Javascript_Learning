8c34cfa9efebe8aae8d300a0799100c12c08ccca
Kevin95580 <163616303+Kevin95580@users.noreply.github.com>
2024-11-28 16:49:59 +0000

    dates

---
+++ 01_Basics/07_dates.js
@@ -0,0 +1,17 @@
+const myDate = new Date();
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleTimeString());
+// console.log(myDate.toTimeString());
+// console.log(typeof myDate);
+
+
+let anotherDate = new Date(2023,12,31);
+console.log(anotherDate.toDateString());
+console.log(anotherDate.tOLPCString());
+
