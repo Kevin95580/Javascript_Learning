8c366e4ffb16cb8b0b8892c8e3d61e3260e8dcf7
Kevin95580 <163616303+Kevin95580@users.noreply.github.com>
2024-12-03 16:19:07 +0000

    date done

--- 01_Basics/07_dates.js
+++ 01_Basics/07_dates.js
@@ -11,7 +11,26 @@ const myDate = new Date();
 // console.log(typeof myDate);
 
 
-let anotherDate = new Date(2023,12,31);
+// let anotherDate = new Date(2023,12,31);
+//in js months start with 0
+// let anotherDate = new Date(2023,0,31);
+// let anotherDate = new Date("2023-01-31");
+let anotherDate = new Date("01-31-2023");
+//mm-dd-yyyy
 console.log(anotherDate.toDateString());
-console.log(anotherDate.tOLPCString());
+console.log(anotherDate.toLocaleString());
+
+
+let myTimeStamp = Date.now();
+// console.log(myTimeStamp);
+// console.log(anotherDate.getTime());
+console.log(Math.round(Date.now()/1000));
+
+let newDate = new Date();
+console.log(newDate.getMonth()+1)//because mothns array so it starts with 0
+console.log(newDate.getMonth())
+
+
+
+
 
