52a5c4b0503fb49561f7813576840a82dfd87d4d
Kevin95580 <163616303+Kevin95580@users.noreply.github.com>
2024-12-04 16:44:23 +0000

    half completed

---
+++ 02_basics/03__objects.js
@@ -0,0 +1,16 @@
+//singleton 
+//when object created as constructor it is singlton
+
+//object literals
+
+const employyes = {
+    fName:"Darshan",
+    lName:"University",
+    email:"darshan@gmail.com",
+    age:15,
+    isLoggedIn: false
+};
+//all the keys considered as string
+
+console.log(employyes.fName);
+console.log(employyes["fName"]);
