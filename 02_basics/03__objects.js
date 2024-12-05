4ee195419b99e9814255291f06ad246e15fc69c1
Kevin95580 <163616303+Kevin95580@users.noreply.github.com>
2024-12-05 12:17:23 +0000

    objects completed

--- 02_basics/03__objects.js
+++ 02_basics/03__objects.js
@@ -3,14 +3,45 @@
 
 //object literals
 
-const employyes = {
+const mySymbol =  Symbol("key1");
+//Symbol - is a datatype in js
+
+const employees = {
     fName:"Darshan",
     lName:"University",
     email:"darshan@gmail.com",
     age:15,
-    isLoggedIn: false
+    isLoggedIn: false,
+    // "mySymbol":"MyKey1",
+    [mySymbol]:"MyKey1"
 };
 //all the keys considered as string
 
-console.log(employyes.fName);
-console.log(employyes["fName"]);
+// console.log(employees.fName);
+// console.log(employees["fName"]);
+// console.log( employees[mySymbol]);
+// console.log(typeof employees[mySymbol]);//**** */
+
+
+
+employees.email = "kevin.kagathara248@gmail.com";
+// console.log(employees);
+// Object.freeze(employees); // this method don't allow you to change the value of any key of object
+employees.email = "darshanUniversity@gmnail.com";
+// console.log(employees);
+
+employees.greeting = function()
+{
+        console.log("Hello World");
+}
+
+employees.greeting2 = function()
+{
+    console.log(`My name is ${this.fName}`);
+    
+}
+
+console.log(employees.greeting); // here only for function reference
+// employees.greeting();
+// employees.greeting2();
+
